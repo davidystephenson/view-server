838d4a62d0760c59b77374a282d971616f157ae2
David Y. Stephenson <david@davidystephenson.com>
2020-02-21 17:55:32 +0100

    Use SWAPI

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
 const express = require('express')
+const axios = require('axios')
 
 const app = express()
 
+const render = (data) => `<html>
+  <head>
+    <title>Star Wars server</title>
+  </head>
+  <body>
+    <h1>${data.name} is a Star Wars character!</h1>
+  </body>
+</html>`
+
 app.get(
-  '/',
-  (request, response) => {
-    response.send('Welcome')
+  '/:characterId',
+  async (req, res) => { // handler callback
+    try {
+      const id = req.params.characterId
+
+      const url = `https://swapi.co/api/people/${id}/`
+
+      console.log('url test:', url)
+
+      const response = await axios.get(url)
+
+      const page = render(response.data)
+
+      res.send(page)
+    } catch (error) {
+      console.log(error)
+    }
   }
 )
 
